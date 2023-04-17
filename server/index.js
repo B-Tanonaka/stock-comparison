64db885d0f7dd8b8ef6d422dd942b0c51d1ef54f
Brett Tanonaka <tano.brett@gmail.com>
2023-04-17 16:09:16 -0700

    Install Airbnb linter

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+const path = require('path');
+const cors = require('cors');
+const router = require('./router');
 const app = express();
+const PORT = process.env.PORT || 4321;
 
 app.use(cors());
+app.use(express.json());
 app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/', router.get);
 
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
