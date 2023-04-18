b7285991af2210e9de2a5398661192a55eaff0db
Brett Tanonaka <tano.brett@gmail.com>
2023-04-18 12:23:20 -0700

    Graph range calculates based on data

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -10,12 +10,30 @@ import {
 const moment = require('moment');
 
 function Graph({ ticker1 }) {
+  const [range, setRange] = useState({ low: Infinity, high: 0 });
+
+  const findRange = () => {
+    ticker1.forEach((day) => {
+      day.price = Math.round(day.price * 100) / 100;
+      if (day.price < range.low) {
+        setRange({ ...range, low: day.price });
+      }
+      if (day.price > range.high) {
+        setRange({ ...range, high: day.price });
+      }
+      console.log(range);
+    });
+    return [range.low, range.high];
+  };
+
+// console.log(ticker1)
+
   return (
     <div>
       <LineChart width={1000} height={300} data={ticker1}>
-        <XAxis dataKey="date" />
-        <YAxis />
-        <CartesianGrid stroke="#eee" strokeDasharray="3 3" />
+        <XAxis />
+        <YAxis dataKey="price" type="number" domain={findRange} />
+        {/* <CartesianGrid stroke="#eee" strokeDasharray="3 3" /> */}
         <Line type="monotone" dataKey="price" stroke="#8884d8" />
       </LineChart>
     </div>
