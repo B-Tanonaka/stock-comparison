58f2c8970ac80ebff488db4d577cbeb21d592ea9
Brett Tanonaka <tano.brett@gmail.com>
2023-04-18 23:14:11 -0700

    Budget dynamically changes both gains

--- client/src/components/BudgetAndResult.jsx
+++ client/src/components/BudgetAndResult.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-const moment = require('moment');
 
 function BudgetAndResult({
   budget,
@@ -20,43 +19,37 @@ function BudgetAndResult({
     }
   };
 
-  // Calculate selected day's shares for ticker1
+  // Calculate total gains for ticker1
   useEffect(() => {
     if (ticker1.data) {
       const t1firstDayPrice = ticker1.data[0][ticker1.name];
       const t1TodayPrice = ticker1.data[ticker1.data.length - 1][ticker1.name];
       const t1shares = budget / t1firstDayPrice;
       const t1totalGains = Math.round(((t1shares * t1TodayPrice) - budget) * 100) / 100;
-      setGains({ ...gains, ticker1: `$${t1totalGains}` });
+      setGains((gains) => { gains.ticker1 = t1totalGains; return gains; });
     }
-  }, [ticker1]);
-
-  console.log('gains: ', gains);
-
-  // Calculate selected day's shares for ticker2
-  useEffect(() => {
     if (ticker2.data) {
       const t2firstDayPrice = ticker2.data[0][ticker2.name];
       const t2TodayPrice = ticker2.data[ticker2.data.length - 1][ticker2.name];
       const t2shares = budget / t2firstDayPrice;
       const t2totalGains = Math.round(((t2shares * t2TodayPrice) - budget) * 100) / 100;
-      setGains({ ...gains, ticker2: `$${t2totalGains}` });
+      setGains((gains) => { gains.ticker2 = t2totalGains; return gains; });
     }
-  }, [ticker2]);
+  }, [ticker1, ticker2, budget]);
 
   return (
     <div>
       <div className="ticker-names">
         <div>
-          <div>{ticker1.name}: {gains.ticker1}</div>
+          <div>{ticker1.name}: {`$${gains.ticker1}`}</div>
           <span onClick={() => resetTicker('1')}>X</span>
         </div>
         <div>
-          <div>{ticker2.name}: {gains.ticker2}</div>
+          <div>{ticker2.name}: {`$${gains.ticker2}`}</div>
           <span onClick={() => resetTicker('2')}>X</span>
         </div>
       </div>
-      <div>{budget}</div>
+      <div>{`$${budget}`}</div>
     </div>
   );
 }
